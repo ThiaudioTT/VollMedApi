42c400f3a5ef496dd90c9c3b2e386b298d64a29b
ThiaudioTT <thiago.sky25@gmail.com>
2024-02-13 10:54:22 -0300

    feat: adds validation for PacienteRecord

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.PacienteRecord;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
 
     @PostMapping
-    public void create(@RequestBody PacienteRecord dados) {
+    public void create(@RequestBody @Valid PacienteRecord dados) {
         System.out.println(dados);
     }
 }
--- src/main/java/med/voll/api/paciente/PacienteRecord.java
+++ src/main/java/med/voll/api/paciente/PacienteRecord.java
@@ -1,11 +1,21 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.EnderecoRecord;
 
 public record PacienteRecord(
+        @NotBlank
         String nome,
+        @NotBlank @Email
         String email,
+        @NotBlank @Pattern(regexp = "\\d{11}")
         String telefone,
+        @NotBlank @Pattern(regexp = "\\d{11}")
         String cpf,
+        @NotNull @Valid
         EnderecoRecord endereco
 ) { }
