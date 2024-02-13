b5a9fa0e7b28d2441dfe28ba821d8a8b8268a45d
ThiaudioTT <thiago.sky25@gmail.com>
2024-02-13 11:20:32 -0300

    feat: WIP do the crud of paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRecord;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     @PostMapping
     public void create(@RequestBody @Valid PacienteRecord dados) {
         System.out.println(dados);
+        this.pacienteRepository.save(new Paciente(dados));
     }
 }
