c12962d075c99dcaf1d9dfe52b961310590ae6c6
ThiaudioTT <thiago.sky25@gmail.com>
2024-02-15 17:17:22 -0300

    feat: adds pagination and some notations

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,9 @@ import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRecord;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,9 +31,11 @@ public class MedicoController {
 
 
     @GetMapping
-    public List<ListagemMedicoRecord> list() {
-        return this.repository.findAll()// return all the medicos
-                .stream().map(ListagemMedicoRecord::new).toList(); // map the medicos to the ListagemMedicoRecord
+    public Page<ListagemMedicoRecord> list(@PageableDefault(size = 10) Pageable pageable) {
+        // with @pageableDefault we can set the default page size
+        // when using pagination, we don't return a list, we return a page
+        return this.repository.findAll(pageable)// return all the medicos
+                .map(ListagemMedicoRecord::new); // map the medicos to the ListagemMedicoRecord
     }
 
 }
