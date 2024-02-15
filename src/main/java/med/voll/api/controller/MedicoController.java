8d4c4322e0fa307aacfae7e80a6af4fb86d7f6d2
ThiaudioTT <thiago.sky25@gmail.com>
2024-02-15 16:49:30 -0300

    feat: creates list medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,14 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.ListagemMedicoRecord;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRecord;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,6 +24,13 @@ public class MedicoController {
     public void create(@RequestBody @Valid MedicoRecord dados) { // @Validate will use bean validation and will validate the object
 //        System.out.println(dados);
         repository.save(new Medico(dados)); // create a new Medico object and save it to the database
+    }
+
+
+    @GetMapping
+    public List<ListagemMedicoRecord> list() {
+        return this.repository.findAll()// return all the medicos
+                .stream().map(ListagemMedicoRecord::new).toList(); // map the medicos to the ListagemMedicoRecord
     }
 
 }
---
+++ src/main/java/med/voll/api/medico/ListagemMedicoRecord.java
@@ -0,0 +1,15 @@
+package med.voll.api.medico;
+
+public record ListagemMedicoRecord(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade
+) { // Returning only the fields that we want to show in the list in this DTO.
+    // It is good to use DTOs to avoid exposing the database model
+
+
+    public ListagemMedicoRecord(Medico medico) { // to convert medico to ListagemMedicoRecord
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
