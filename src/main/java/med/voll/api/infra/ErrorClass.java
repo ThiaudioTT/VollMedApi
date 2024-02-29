d5ee53684289a2323edc8742b6cfcff4c180362a
ThiaudioTT <thiago.sky25@gmail.com>
2024-02-29 15:54:27 -0300

    feat: adds errorClass for 400

--- src/main/java/med/voll/api/infra/ErrorClass.java
+++ src/main/java/med/voll/api/infra/ErrorClass.java
@@ -3,8 +3,12 @@ package med.voll.api.infra;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice // This annotation is used to handle exceptions in the whole application
 public class ErrorClass {
@@ -15,5 +19,19 @@ public class ErrorClass {
 //        return ResponseEntity.notFound().build();
         // todo: change this to a JSON response (optional)
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    // 400 Bad Request
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handle400(MethodArgumentNotValidException exception) {
+        List<FieldError> errors = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(Error::new).toList());
+    }
+
+    public record Error(String field, String message) {
+        public Error(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
     }
 }
