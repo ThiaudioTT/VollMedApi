5d19b01fcde43452418cbf9def86886c756cc510
ThiaudioTT <thiago.sky25@gmail.com>
2024-05-29 16:03:22 -0300

    feat(consultas): add ConsultaRequestDTO.java

--- src/main/java/med/voll/api/controller/ConsultasController.java
+++ src/main/java/med/voll/api/controller/ConsultasController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consultas.ConsultaRequestDTO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,7 @@ public class ConsultasController {
 
     @PostMapping("/agendar")
     @Transactional
-    public void agendarConsulta() {
+    public void agendarConsulta(@RequestBody @Valid ConsultaRequestDTO consulta) {
 
     }
 }
---
+++ src/main/java/med/voll/api/domain/consultas/ConsultaRequestDTO.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consultas;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record ConsultaRequestDTO(
+
+        // not blank is used with strings to validate if the string is not null and not empty
+        @NotBlank(message = "Nome do paciente é obrigatório") String nomePaciente,
+        @NotBlank(message = "Nome do médico é obrigatório") String nomeMedico,
+
+        @NotNull(message = "Data da consulta é obrigatória")LocalDateTime dataConsulta
+
+        ) {
+}
