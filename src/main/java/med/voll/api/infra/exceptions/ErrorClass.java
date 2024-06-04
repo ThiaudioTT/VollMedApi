f3982c796b20c477463d842ca8363ab55af8a42a
ThiaudioTT <thiago.sky25@gmail.com>
2024-06-04 13:17:43 -0300

    feat(errorClass): adds error 500 and fix error 404

--- src/main/java/med/voll/api/infra/exceptions/ErrorClass.java
+++ src/main/java/med/voll/api/infra/exceptions/ErrorClass.java
@@ -7,15 +7,15 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import java.util.List;
 
 @RestControllerAdvice // This annotation is used to handle exceptions in the whole application
 public class ErrorClass {
 
-    @ExceptionHandler({EntityNotFoundException.class})
-    public ResponseEntity<String> handle404(EntityNotFoundException ex) {
-        // This method will handle 404 errors
+    // this method will handle 404 errors
+    @ExceptionHandler({EntityNotFoundException.class, NoResourceFoundException.class})
+    public ResponseEntity<String> handle404(Exception ex) {
 //        return ResponseEntity.notFound().build();
         // todo: change this to a JSON response (optional)
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
@@ -27,6 +27,13 @@ public class ErrorClass {
         List<FieldError> errors = exception.getFieldErrors();
 
         return ResponseEntity.badRequest().body(errors.stream().map(Error::new).toList());
+    }
+
+    // 500 Internal Server Error
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle500(Exception exception) {
+        System.out.println(exception.getClass());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 
     public record Error(String field, String message) {
