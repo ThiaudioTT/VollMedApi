e0d429f72e8ee0a2296e8f85801d67d1026cfa72
ThiaudioTT <thiago.sky25@gmail.com>
2024-06-04 13:26:07 -0300

    chore(consultas): working on consultas

--- src/main/java/med/voll/api/controller/ConsultasController.java
+++ src/main/java/med/voll/api/controller/ConsultasController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consultas.ConsultaRequestDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,9 @@ public class ConsultasController {
 
     @PostMapping("/agendar")
     @Transactional
-    public void agendarConsulta(@RequestBody @Valid ConsultaRequestDTO consulta) {
+    public ResponseEntity<Void> agendarConsulta(@RequestBody @Valid ConsultaRequestDTO consulta) {
         System.out.println("Agendando consulta: " + consulta.toString());
+
+        return ResponseEntity.ok().build();
     }
 }
