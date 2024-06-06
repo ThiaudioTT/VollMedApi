8691a0dc15a7ce2b86eaf5c022d947e90be5fe43
ThiaudioTT <thiago.sky25@gmail.com>
2024-06-06 16:20:02 -0300

    feat(agendaconsulta): WIP working on agenda consultas

--- src/main/java/med/voll/api/domain/consultas/AgendaConsultasService.java
+++ src/main/java/med/voll/api/domain/consultas/AgendaConsultasService.java
@@ -1,12 +1,34 @@
 package med.voll.api.domain.consultas;
 
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendaConsultasService {
 
+    @Autowired
+    private ConsultaRepository consultaRepo;
+
+    @Autowired
+    private MedicoRepository medicoRepo;
+
+    @Autowired
+    private PacienteRepository pacienteRepo;
+
     // this is where the business logic should be
     public void agendarConsulta(ConsultaRequestDTO consulta) {
-        System.out.println("NOT implemented: " + consulta.toString());
+
+        // using .orElseThrow() to throw an exception if the id is not found is better than using .get() who throws a NoSuchElementException
+        // this way we can throw a custom exception and give a message to the user
+        this.consultaRepo.save(new Consulta(
+                null,
+                this.medicoRepo.findById(consulta.idMedico())
+                                .orElseThrow(() -> new RuntimeException("Médico não encontrado")),
+                this.pacienteRepo.findById(consulta.idPaciente())
+                        .orElseThrow(() -> new RuntimeException("Paciente não encontrado")),
+                consulta.dataConsulta()
+        ));
     }
 }
--- src/main/java/med/voll/api/domain/consultas/ConsultaRequestDTO.java
+++ src/main/java/med/voll/api/domain/consultas/ConsultaRequestDTO.java
@@ -11,8 +11,10 @@ import java.time.LocalDateTime;
 public record ConsultaRequestDTO(
 
         // not blank is used with strings to validate if the string is not null and not empty
-        @NotBlank(message = "Nome do paciente é obrigatório") String nomePaciente,
-        @NotBlank(message = "Nome do médico é obrigatório") String nomeMedico,
+//        @NotBlank(message = "Nome do paciente é obrigatório") String nomePaciente,
+//        @NotBlank(message = "Nome do médico é obrigatório") String nomeMedico,
+        @NotNull(message = "Id do paciente é obrigatório") Long idPaciente,
+        @NotNull(message = "Id do médico é obrigatório") Long idMedico,
 
         @NotNull(message = "Data da consulta é obrigatória")
         @Future(message = "Data da consulta deve ser futura")
